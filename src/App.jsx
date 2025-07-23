11ca49e2a978b17f8ed574ca7bd0e9ec28cd05ce
mikewanjero <mikewanj@gmail.com>
2025-07-23 22:48:29 +0300

    Route paths

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,26 @@ import ViewInventory from './components/ViewInventory';
 import EditInventory from './components/EditInventory';
 import DeleteInventory from './components/DeleteInventory';
 import { CContainer, CNavbar, CNavItem, CNavLink } from '@coreui/react';
+import Header from './components/Navigation/Header';
+import Sidebar from './components/Navigation/Sidebar';
 
 function App() {
   return (
     <Router>
-      <CNavbar colorScheme="light" className="mb-4">
-        <CNavItem><CNavLink component={Link} to="/">Create</CNavLink></CNavItem>
-        <CNavItem><CNavLink component={Link} to="/view">View</CNavLink></CNavItem>
-        <CNavItem><CNavLink component={Link} to="/edit">Edit</CNavLink></CNavItem>
-        <CNavItem><CNavLink component={Link} to="/delete">Delete</CNavLink></CNavItem>
-      </CNavbar>
-
-      <CContainer>
-        <Routes>
-          <Route path="/" element={<CreateInventory />} />
-          <Route path="/view" element={<ViewInventory />} />
-          <Route path="/edit" element={<EditInventory id={1} />} /> {/* Replace 1 with dynamic ID later */}
-          <Route path="/delete" element={<DeleteInventory id={1} />} />
-        </Routes>
-      </CContainer>
+      <div className="app-layout">
+        <Sidebar />
+        <div className="main-content">
+          <Header />
+          <CContainer className='pt-4'>
+            <Routes>
+              <Route path="/" element={<CreateInventory />} />
+              <Route path="/view" element={<ViewInventory />} />
+              <Route path="/edit" element={<EditInventory id={1} />} /> 
+              <Route path="/delete" element={<DeleteInventory id={1} />} />
+            </Routes>
+          </CContainer>
+        </div>
+      </div>
     </Router>
   );
 }
