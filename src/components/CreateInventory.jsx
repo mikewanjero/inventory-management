802d9ea63c49048b4ea6933123eedb1c7c626f81
mikewanjero <mikewanj@gmail.com>
2025-07-24 10:47:44 +0300

    create item layout

--- src/components/CreateInventory.jsx
+++ src/components/CreateInventory.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { createInventory } from '../services/apiSetup';
-import { CButton, CFormInput, CModal, CModalBody, CModalFooter, CModalHeader, CToast, CToaster } from '@coreui/react';
+import { CButton, CContainer, CFormInput, CModal, CModalBody, CModalFooter, CModalHeader, CToast, CToaster } from '@coreui/react';
 
 export default function CreateInventory() {
     const [item, setItem] = useState({ name: '', quantity : '' });
@@ -22,14 +22,15 @@ export default function CreateInventory() {
 
   return (
     <>
-        <CButton onClick={() => setModalIsVisible(true)}>Create Inventory Item</CButton>
-        <CModal visible={modalIsVisible} onClose={() => setModalIsVisible(false)}>
-            <CModalHeader>New Inventory Item</CModalHeader>
-            <CModalBody>
+        <CButton className='create-btn' onClick={() => setModalIsVisible(true)}>Create Inventory Item</CButton>
+        <CModal className='create-modal' visible={modalIsVisible} onClose={() => setModalIsVisible(false)}>
+            <CModalHeader className='modal-header'>New Inventory Item</CModalHeader>
+            <CModalBody className='modal-body'>
                 <CFormInput 
                     type='text' 
                     label='Name'
                     value={item.name}
+                    className='modal-input'
                     onChange={(e) => {
                         setItem({ ...item, name: e.target.value});
                     }}/>
@@ -37,14 +38,15 @@ export default function CreateInventory() {
                     type='Number'
                     label='Quantity'
                     value={item.quantity}
+                    className='modal-input'
                     onChange={(e) => {
                         setItem({ ...item, quantity: e.target.value});
                     }}/>
+                <CContainer className='modal-buttons'>
+                    <CButton color='primary' onClick={handleSubmit}>Create</CButton>
+                    <CButton color='secondary' onClick={() => setModalIsVisible(false)}>Cancel</CButton>
+                </CContainer>
             </CModalBody>
-            <CModalFooter>
-                <CButton color='primary' onClick={handleSubmit}>Create</CButton>
-                <CButton color='secondary' onClick={() => setModalIsVisible(false)}>Cancel</CButton>
-            </CModalFooter>
         </CModal>
 
         <CToaster placement="top-end">
