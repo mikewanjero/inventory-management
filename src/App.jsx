b3ad758eb5c1d5f19d82372f20fb05b5174cffd2
mikewanjero <mikewanj@gmail.com>
2025-07-28 10:41:22 +0300

    new component `ToastProvider`

--- src/App.jsx
+++ src/App.jsx
@@ -2,28 +2,29 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
 import CreateInventory from './components/CreateInventory';
 import ViewInventory from './components/ViewInventory';
-import EditInventory from './components/EditInventory';
-import DeleteInventory from './components/DeleteInventory';
-import { CContainer, CNavbar, CNavItem, CNavLink } from '@coreui/react';
+import { CContainer } from '@coreui/react';
 import Header from './components/Navigation/Header';
 import Sidebar from './components/Navigation/Sidebar';
+import { ToastProvider } from './components/ToastProvider';
 
 function App() {
   return (
-    <Router>
-      <div className="app-layout">
-        <Sidebar />
-        <div className="main-content">
-          <Header />
-          <CContainer className='pt-4'>
-            <Routes>
-              <Route path="/" element={<CreateInventory />} />
-              <Route path="/view" element={<ViewInventory />} />
-            </Routes>
-          </CContainer>
+    <ToastProvider>
+      <Router>
+        <div className="app-layout">
+          <Sidebar />
+          <div className="main-content">
+            <Header />
+            <CContainer className='pt-4'>
+              <Routes>
+                <Route path="/" element={<CreateInventory />} />
+                <Route path="/view" element={<ViewInventory />} />
+              </Routes>
+            </CContainer>
+          </div>
         </div>
-      </div>
-    </Router>
+      </Router>
+    </ToastProvider>
   );
 }
 
