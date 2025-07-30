8df094862aff63e01b56848aa2eb245c10b0f431
mikewanjero <mikewanj@gmail.com>
2025-07-30 13:14:39 +0300

    header styles

--- src/components/Navigation/Header.jsx
+++ src/components/Navigation/Header.jsx
@@ -3,7 +3,7 @@ import { CHeader, CHeaderBrand } from '@coreui/react'
 
 export default function Header() {
   return (
-    <CHeader className='header bg-light'>
+    <CHeader className='header'>
         <div className="header-content">
           Inventory Management
         </div>
